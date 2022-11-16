5b2f6902825c567c70165f6b53d0f72aba14a1c7
Bilel <bilel77127@icloud.com>
2022-11-16 16:24:27 +0900

    test

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+const path = require('path')
+require('dotenv').config({ path: path.resolve('/Users/bileltamouneit/Desktop/cc/aws/.env') });
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
